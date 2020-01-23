efff419686143f7283886cbc8c40c0d1392e17b1
Adam Gasiewski <tug99025@temple.edu>
2020-01-23 10:31:23 -0500

    working on wcat

---
+++ initial-utilities/wcat/mainn.c
@@ -0,0 +1,11 @@
+//
+//  mainn.c
+//  
+//
+//  Created by Adam Gasiewski on 1/22/20.
+//
+
+#include "mainn.h"
+hello
+hi
+hello
--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,2 +1,20 @@
-prompt> ./wcat main.c
+
 #include <stdio.h>
+
+main() {
+
+FILE *fp = fopen("main.c", "r");
+if (fp == NULL) {
+    printf("wcat: cannot open file\n");
+    exit(1);
+}
+
+int bufferLength = 255;
+char buffer[bufferLength];
+
+while(fgets(buffer, bufferLength, fp)) {
+    printf("%s", buffer);
+}
+fclose(fp);
+    return 0;
+}
