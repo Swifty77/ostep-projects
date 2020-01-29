d8143c16113e18d700ba708ee28d719f90f54efe
Adam Gasiewski <tug99025@temple.edu>
2020-01-29 00:02:59 -0500

    Working on wgrep- handling edge cases

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
     
 char line [100];
-FILE *fp = fopen(argv[], "r");
+FILE *fp;
+    
+    if (argc < 2) {
+        printf("wgrep: searchterm [file ...]");
+        exit(1);
+    }
+    
+    for (int i = 0; i < argc; i++){
+        fp = fopen(argv[i], "r");
+        
+        if (fp == NULL) {
+            printf("wgrep: cannot open file");
+            exit(1);
+        }
+        
+        while 
+    }
+    
+    
+    
+//    = fopen(argv[], "r");
     
 
     
     
     
     
-    
+    return 0;
 }
