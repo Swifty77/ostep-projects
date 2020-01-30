262247a75f64e5b96d9b4e671ad425aedf69bd88
Adam Gasiewski <tug99025@temple.edu>
2020-01-29 23:16:56 -0500

    added documentation

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -6,10 +6,10 @@ int main(int argc, char** argv) {
     
     if (argc == 0)
         exit(0);
-    
+    //scans all files
 for (int fileNum = 1; fileNum < argc; fileNum++) {
 
-FILE *fp = fopen(argv[fileNum], "r");
+FILE *fp = fopen(argv[fileNum], "r"); //opens file
 if (fp == NULL) {
     printf("wcat: cannot open file\n");
     exit(1);
@@ -18,6 +18,7 @@ if (fp == NULL) {
 int bufferLength = 255;
 char buffer[bufferLength];
 
+    //prints each line
 while(fgets(buffer, bufferLength, fp)) {
     printf("%s", buffer);
 }
