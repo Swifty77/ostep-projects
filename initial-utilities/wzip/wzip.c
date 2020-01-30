2ab0e03de06b43bda513270007b2382520d5deda
Adam Gasiewski <tug99025@temple.edu>
2020-01-29 19:54:43 -0500

    Working wzip (I think)

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 
 int main (int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]");
+        exit(1);
+    }
+    
+    int totalCars;
+    char car, lastCar = '\0';
+    size_t byteSize = 4;
+    size_t memb = 1;
+    for (int i = 1; i< argc; i++){
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wzip: cannot open file");
+            exit(1);
+        }
+        
+        if (lastCar == '\0'){
+            lastCar = getc(fp);
+        }
+        while ((car = getc(fp)) != EOF){
+           
+            if (car == lastCar) {
+                totalCars++;
+            }
+            else {
+                fwrite(&totalCars, byteSize, memb, stdout);
+                fwrite(&lastCar, memb, memb, stdout);
+                totalCars = 1;
+            }
+            lastCar = car;
+        }
+        
+        if (i == argc - 1) {
+            fwrite(&totalCars, byteSize, memb, stdout);
+            fwrite(&lastCar, memb, memb, stdout);
+            totalCars = 1;
+        }
+        fclose(fp);
+        
+    }
+    
     
     
     
